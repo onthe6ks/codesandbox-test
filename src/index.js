80161846a525e76236de09c46a59061e27498c9b
onthe6ks <onthe6ks@gmail.com>
2022-02-18 16:42:14 +0900

    learn variable let,const

--- src/index.js
+++ src/index.js
@@ -1,12 +1,54 @@
-import "./styles.css";
+// /**
+//  *  const let等の変数宣言
+//  */
 
-console.log("ああああ");
+// /**
+//  * コメントの記載方法
+//  */
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// var val1 = "val1変数";
+// console.log(val1);
+
+// //varは変数の上書きが可能
+// val1 = "変数の上書き";
+// console.log(val1);
+
+// //var変数の再宣言
+// var val1 = "var変数の再宣言";
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// //letは上書き可能
+// val2 = "上書き";
+// console.log(val2);
+
+// //letは再宣言不可 errorになる
+// let val2 = "hoge";
+
+// const val3 = "const変数";
+// console.log(val3);
+
+// constは上書きと再宣言不可
+// val3 = "上書き";
+// const val3 = "hoge";
+
+// constで定義したオブジェクトはプロパティの変更が可能
+// const val4 = {
+//   name: "hoge",
+//   age: 28,
+// };
+
+// //オブジェトであれば、変更はできる
+// val4.name = "abcd";
+// val4.address = "tokyo"
+
+// console.log(val4);
+
+// //オブジェトであれば、変更はできる
+// const val5 = ["dog", "cat"];
+// val5[0] = "bird";
+// val5.push("monkey");
+
+// console.log(val5);
